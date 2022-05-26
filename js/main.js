56698a81cf4eb8c1830a51f3d8cfdc0be9834882
HaroldSPT <hspatino2@misena.edu.co>
2022-05-26 17:29:15 -0500

    Mostrar la información real del usuario

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 const API = "https://api.github.com/users/";
 
 const app = Vue.createApp({
-    data() {
-        return {
-          busqueda: null,
-          result: null
-        }
-      },
-    //La palabra function ya no es necesaria pues se usa un método
-    methods: {
-      async Buscar() {
+  data() {
+    return {
+      busqueda: null,
+      result: null,
+      error: null
+    }
+  },
+  //La palabra function ya no es necesaria pues se usa un método
+  methods: {
+    async Buscar() {
+      //
+      this.result = this.error = null
+      try {
         const response = await fetch(API + this.busqueda)
+        if(!response.ok) throw new Error("Usuario no econtrado")
         const data = await response.json()
         console.log(data)
-        this.result = true
+        this.result = data
+      } catch (error) {
+        this.error = error
+        //tan pronto termina el proceso limpia la búsqueda.
+      } finally {
+        this.busqueda
       }
+
     }
+  }
 })
